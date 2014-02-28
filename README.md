15ed25057ebeee43950b98f6fc43816ebfbe0bbc
J Shamsul Bahri <jibone@gmail.com>
2014-03-01 01:29:52 +0800

    update bootstrap file

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Setup a Vagrant box with basic LAMP stack.
+
+Get a basic Ubuntu base box.
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,23 +1,34 @@
 #!/bin/sh
 apt-get update
+apt-get upgrade
 apt-get install -y build-essential openssl curl zlib1g zlib1g-dev libssl-dev libyaml-dev libxml2-dev libxslt-dev autoconf libc6-dev ncurses-dev automake libtool bison git-core git
 apt-get install -y vim
 echo mysql-server mysql-server/root_password select "vagrant" | debconf-set-selections
 echo mysql-server mysql-server/root_password_again select "vagrant" | debconf-set-selections
 apt-get install -y mysql-server 
 apt-get install -y apache2
+apt-get install -y python-software-properties
+add-apt-repository ppa:ondrej/php5
+apt-get update
 apt-get install -y php5 php5-cli php5-mcrypt php5-mysql libapache2-mod-php5
 VHOST=$(cat <<EOF
 <VirtualHost *:80>
-  DocumentRoot "/vagrant/web"
+  DocumentRoot "/vagrant/web/public"
   ServerName localhost
-  <Directory "/vagrant/web">
+  <Directory "/vagrant/web/public">
     AllowOverride All
   </Directory>
 </VirtualHost>
 EOF
 )
-echo "${VHOST}" > /etc/apache2/sites-enable/000-default
-sudo a2enmod rewrite
+touch /etc/apache2/sites-available/web
+echo "${VHOST}" > /etc/apache2/sites-available/web
+echo "ServerName localhost" > /etc/apache2/httpd.conf
+mkdir /vagrant/web
+mkdir /vagrant/web/public
+echo "<?php phpinfo(); ?>" > /vagrant/web/public/index.php
+sudo a2enmod rewrite actions fastcgi alias
+sudo a2dissite default
+sudo a2ensite web
 sudo service apache2 restart
 apt-get clean
